27c2c5695891d087c477289c98f62554a5904836
Sangyao42 <wangsangyao@hotmail.com>
2023-08-21 16:59:26 +0200

    Added[cpp & hpp]: tests needed in the main

---
+++ ex01/src/ClapTrap.cpp
@@ -0,0 +1,127 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ClapTrap.cpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sawang <sawang@student.42heilbronn.de>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/20 00:21:07 by sawang            #+#    #+#             */
+/*   Updated: 2023/08/21 16:45:36 by sawang           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ClapTrap.hpp"
+
+#include <iostream>
+
+ClapTrap::ClapTrap(): _name("ClapTrap"), _hitPoints(10), _energyPoints(10), _attackDamage(0)
+{
+	std::cout << "ClapTrap default constructor is called." << std::endl;
+}
+
+ClapTrap::ClapTrap(const std::string name) : _name(name), _hitPoints(10), _energyPoints(10), _attackDamage(0)
+{
+	std::cout << "ClapTrap " << this->_name << " is born!" << std::endl;
+}
+
+ClapTrap::ClapTrap(const ClapTrap &copy)
+{
+	std::cout << "copy constructor is called." << std::endl;
+	*this = copy;
+}
+
+ClapTrap &ClapTrap::operator=(const ClapTrap &rhs)
+{
+	std::cout << "copy assignment is called." << std::endl;
+	if (this != &rhs)
+	{
+		this->_name = rhs._name;
+		this->_hitPoints = rhs._hitPoints;
+		this->_energyPoints = rhs._energyPoints;
+		this->_attackDamage = rhs._attackDamage;
+	}
+	return (*this);
+}
+
+ClapTrap::~ClapTrap()
+{
+	std::cout << "ClapTrap " << this->_name << " is destoyed!" << std::endl;
+}
+
+void	ClapTrap::attack(const std::string &target)
+{
+	if (this->_hitPoints == 0)
+	{
+		std::cout << "ClapTrap " << this->_name << " is already dead! Cannot attack!" << std::endl;
+		return ;
+	}
+	else if (this->_energyPoints == 0)
+	{
+		std::cout << "ClapTrap " << this->_name << " has no energy to attack!" << std::endl;
+		return ;
+	}
+	else
+	{
+		std::cout << "ClapTrap " << this->_name << " attacks " << target << ", causing " << this->_attackDamage << " points of damage!" << std::endl;
+		this->_energyPoints--;
+	}
+}
+
+void	ClapTrap::takeDamage(unsigned int amount)
+{
+	if (this->_hitPoints == 0)
+	{
+		std::cout << "ClapTrap " << this->_name << " is already dead! Cannot take more damage!" << std::endl;
+		return ;
+	}
+	else if (this->_hitPoints <= amount)
+	{
+		std::cout << "ClapTrap " << this->_name << " takes " << amount << " points of damage!" << std::endl;
+		this->_hitPoints = 0;
+	}
+	else
+	{
+		std::cout << "ClapTrap " << this->_name << " takes " << amount << " points of damage!" << std::endl;
+		this->_hitPoints -= amount;
+	}
+}
+
+void	ClapTrap::beRepaired(unsigned int amount)
+{
+	if (this->_hitPoints == 0)
+	{
+		std::cout << "ClapTrap " << this->_name << " is already dead! Cannot be repaired!" << std::endl;
+		return ;
+	}
+	else if(this->_energyPoints == 0)
+	{
+		std::cout << "ClapTrap " << this->_name << " has no energy to be repaired!" << std::endl;
+		return ;
+	}
+	else if (this->_hitPoints == 10)
+	{
+		std::cout << "ClapTrap " << this->_name << " is already fully repaired!" << std::endl;
+		this->_energyPoints--;
+		return ;
+	}
+	else
+	{
+		std::cout << "ClapTrap " << this->_name << " is repaired " << amount << " points of damage!" << std::endl;
+		this->_energyPoints--;
+		this->_hitPoints += amount;
+		if (this->_hitPoints > 10)
+			this->_hitPoints = 10;
+	}
+}
+
+void	ClapTrap::setAttackDamage(int amount)
+{
+	std::cout << "ClapTrap " << this->_name << " has now " << amount << " attack damage!" << std::endl;
+	this->_attackDamage = amount;
+}
+
+unsigned int	ClapTrap::getAttackDamage(void) const
+{
+	return (this->_attackDamage);
+}
+
---
+++ ex01/src/main.cpp
@@ -0,0 +1,57 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sawang <sawang@student.42heilbronn.de>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/20 00:55:11 by sawang            #+#    #+#             */
+/*   Updated: 2023/08/21 16:24:16 by sawang           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ClapTrap.hpp"
+#include "ScavTrap.hpp"
+
+#include <iostream>
+
+int	main(void)
+{
+	ClapTrap	Jim("Jim");
+	ClapTrap	Bob("Bob");
+	ClapTrap	John("John");
+
+	Jim.setAttackDamage(5);
+	for (int i = 0; i < 4; i++)
+	{
+		Jim.attack("Bob");
+		Bob.takeDamage(Jim.getAttackDamage());
+		Bob.beRepaired(1);
+	}
+
+	std::cout << std::endl;
+
+	John.setAttackDamage(4);
+	for (int i = 0; i < 4; i++)
+	{
+		John.attack("Jim");
+		Jim.takeDamage(John.getAttackDamage());
+		Jim.beRepaired(1);
+		Jim.beRepaired(1);
+		Jim.beRepaired(1);
+		Jim.beRepaired(1);
+		Jim.beRepaired(1);
+		Jim.beRepaired(1);
+		Jim.beRepaired(1);
+	}
+
+	/**
+	 * ScavTrap test
+	*/
+	std::cout << std::endl;
+	ScavTrap	ScavJim("ScavJim");
+	ScavTrap	ScavBob("ScavBob");
+	ScavTrap	ScavJohn("ScavJohn");
+
+	return (0);
+}
